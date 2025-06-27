976f30c2690e90660ba760e2da54c835a76d3f16
Ryan Tate <ryan.tate@cirrusaircraft.com>
2025-06-27 16:35:28 -0400

    allow overriding site prefix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+const assetPrefix = process.env.NEXT_PUBLIC_ASSET_PREFIX || basePath || "";
+
 const nextConfig: NextConfig = {
   output: "standalone",
+  basePath: basePath,
+  assetPrefix: assetPrefix,
 };
 
 export default nextConfig;
